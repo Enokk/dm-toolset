8434cbd537e4b029c084d0508fa1b31d81498e21
Enokk <enokk@gmail.com>
2025-07-16 16:46:39 +0200

    inizializzato progetto

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col items-center h-screen mt-10 gap-10">
+    <div class="text-5xl font-bold">
+        Home
+    </div>
+    <Button label="Click" />
+  </div>
+</template>
